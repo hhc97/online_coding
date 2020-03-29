18f2dc998dc160af0d4721709d00bc7531baff0d
Haocheng Hu <hc.hu@utoronto.ca>
2020-03-29 00:52:47 -0400

    fizzbuzz solution

---
+++ Kattis/fizzbuzz.py
@@ -0,0 +1,22 @@
+# https://open.kattis.com/problems/fizzbuzz
+# accepted answer, CPU time: 0.05s
+
+if __name__ == '__main__':
+    from sys import stdin, stdout
+
+    first = stdin.readline().split()
+    fizz, buzz, n = int(first[0]), int(first[1]), int(first[2])
+    output = []
+    for i in range(1, n + 1):
+        is_fizz = i % fizz == 0
+        is_buzz = i % buzz == 0
+        if is_fizz and is_buzz:
+            output.append('FizzBuzz')
+            continue
+        elif is_fizz:
+            output.append('Fizz')
+        elif is_buzz:
+            output.append('Buzz')
+        else:
+            output.append(str(i))
+    stdout.write('\n'.join(output))
