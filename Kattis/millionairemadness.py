97a91a610ae9b16f137a105683a0e56ebbb7387e
Haocheng Hu <hc.hu@utoronto.ca>
2020-04-15 02:52:10 -0400

    add adjacency list function

--- Kattis/millionairemadness.py
+++ Kattis/millionairemadness.py
@@ -10,6 +10,24 @@ def _get_numbers():
     return numbers if len(numbers) > 1 else numbers[0]
 
 
+def make_adjacency_list(grid):
+    d = {}
+    max_row, max_col = len(grid), len(grid[0])
+    for i in range(max_row):
+        for j in range(max_col):
+            neighbors = []
+            for i2 in range(i - 1, i + 2):
+                for j2 in range(j - 1, j + 2):
+                    if i2 != i or j2 != j:
+                        if 0 <= i2 < max_row and 0 <= j2 < max_col:
+                            if i2 == i:
+                                neighbors.append(grid[i2][j2])
+                            if j2 == j:
+                                neighbors.append(grid[i2][j2])
+            d[grid[i][j]] = neighbors
+    return d
+
+
 if __name__ == '__main__':
     from sys import stdin, stdout
 
