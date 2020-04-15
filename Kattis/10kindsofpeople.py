4f76f34691d3d7c52c7343ba36cac57b5ad6081f
Haocheng Hu <hc.hu@utoronto.ca>
2020-04-15 01:05:02 -0400

    faster but still slow

--- Kattis/10kindsofpeople.py
+++ Kattis/10kindsofpeople.py
@@ -11,36 +11,23 @@ def _get_numbers():
 
 
 def _get_row():
-    return [int(i) for i in stdin.readline().strip()]
+    return [int(v) for v in stdin.readline().strip()]
 
 
 def _get_map_copy():
     return [row[:] for row in original_map]
 
 
-def _color(r, c, grid, original_color, color_value):
+def _color(r, c, grid, orig, color_value):
     if 0 <= r < len(grid) and 0 <= c < len(grid[0]):
-        if grid[r][c] != original_color or grid[r][c] == color_value:
+        if grid[r][c] != orig or grid[r][c] == color_value:
             return
-        if grid[r][c] == original_color:
+        if grid[r][c] == orig:
             grid[r][c] = color_value
-            _color(r - 1, c, grid, original_color, color_value)
-            _color(r + 1, c, grid, original_color, color_value)
-            _color(r, c - 1, grid, original_color, color_value)
-            _color(r, c + 1, grid, original_color, color_value)
-
-
-def is_connected(r1, c1, r2, c2):
-    color_map = _get_map_copy()
-    start_value = color_map[r1][c1]
-    _color(r1, c1, color_map, start_value, 5)
-    if color_map[r2][c2] == 5:
-        if start_value == 0:
-            return 'binary'
-        else:
-            return 'decimal'
-    else:
-        return 'neither'
+            _color(r - 1, c, grid, orig, color_value)
+            _color(r + 1, c, grid, orig, color_value)
+            _color(r, c - 1, grid, orig, color_value)
+            _color(r, c + 1, grid, orig, color_value)
 
 
 if __name__ == '__main__':
@@ -53,15 +40,27 @@ if __name__ == '__main__':
     for _ in range(rows):
         original_map.append(_get_row())
 
+    color_map = _get_map_copy()
+    current_color = 2
+    for i in range(len(color_map)):
+        for j in range(len(color_map[0])):
+            if color_map[i][j] <= 1:
+                _color(i, j, color_map, original_map[i][j], current_color)
+                current_color += 1
+
     for _ in range(_get_numbers()):
         row1, col1, row2, col2 = _get_numbers()
         row1 -= 1
         col1 -= 1
         row2 -= 1
         col2 -= 1
-        if original_map[row1][col1] != original_map[row2][col2]:
-            output.append('neither')
+        original_color = original_map[row1][col1]
+        if color_map[row1][col1] == color_map[row2][col2]:
+            if original_color == 1:
+                output.append('decimal')
+            else:
+                output.append('binary')
         else:
-            output.append(is_connected(row1, col1, row2, col2))
+            output.append('neither')
 
     stdout.write("\n".join(output))
