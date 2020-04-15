7cc86577ba886715277f01051f38a843752bc1a4
Haocheng Hu <hc.hu@utoronto.ca>
2020-04-14 23:37:55 -0400

    update docstring

--- Kattis/kattis_template.py
+++ Kattis/kattis_template.py
@@ -1,10 +1,10 @@
 # https://open.kattis.com/problems/PROBLEM_ID
 
 
-def _get_numbers() -> list:
+def _get_numbers():
     """
-    Gets a line of input from stdin and returns a list
-    of it's individual ints.
+    Gets a line of input from stdin and return the numbers in a list.
+    If there is only 1 number, return the number itself.
     """
     numbers = [int(v) for v in stdin.readline().split()]
     return numbers if len(numbers) > 1 else numbers[0]
