a3e1dc2c9e37bb88486044f4217c20298005b046
Haocheng Hu <hc.hu@utoronto.ca>
2020-04-25 05:07:59 -0400

    much simpler solution

--- Kattis/dicecup.py
+++ Kattis/dicecup.py
@@ -19,39 +19,20 @@ Sample output:
 
 """
 
-
-def _get_numbers():
-    """
-    Gets a line of input from stdin and return the numbers in a list.
-    If there is only 1 number, return the number itself.
-    """
-    numbers = [int(v) for v in stdin.readline().split()]
-    return numbers if len(numbers) > 1 else numbers[0]
-
-
 if __name__ == '__main__':
     from sys import stdin, stdout
 
     output = []
-    probabilities = {}
 
-    a, b = _get_numbers()
-    p_a, p_b = 1 / a, 1 / b
+    first = stdin.readline().split()
+    a, b = int(first[0]), int(first[1])
+    if a > b:
+        high = a
+        low = b
+    else:
+        low = a
+        high = b
+    for i in range(low + 1, high + 2):
+        output.append(str(i))
 
-    for i in range(1, a + 1):
-        for j in range(1, b + 1):
-            outcome = i + j
-            p_outcome = p_a * p_b
-            if outcome in probabilities:
-                probabilities[outcome] += p_outcome
-            else:
-                probabilities[outcome] = p_outcome
-
-    p_max = max(probabilities.values())
-    for value, p in probabilities.items():
-        if p == p_max:
-            output.append(value)
-
-    output.sort()
-    output = [str(v) for v in output]
     stdout.write("\n".join(output))
