0f6076bbd212b93221bc6055add960e765b935f5
Haocheng Hu <hc.hu@utoronto.ca>
2020-04-28 04:58:52 -0400

    update template to specify return type

--- Kattis/kattis_template.py
+++ Kattis/kattis_template.py
@@ -10,13 +10,13 @@ Sample output:
 """
 
 
-def _get_numbers():
+def _get_numbers(first=False):
     """
     Gets a line of input from stdin and return the numbers in a list.
     If there is only 1 number, return the number itself.
     """
     numbers = [int(v) for v in stdin.readline().split()]
-    return numbers if len(numbers) > 1 else numbers[0]
+    return numbers[0] if first else numbers
 
 
 if __name__ == '__main__':
