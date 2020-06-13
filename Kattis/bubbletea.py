1c01c1770b24290899a6fc999da38d9a1adc1ae4
Haocheng Hu <hc.hu@utoronto.ca>
2020-06-13 12:49:30 -0400

    this was... rather confusing

--- Kattis/bubbletea.py
+++ Kattis/bubbletea.py
@@ -1,5 +1,5 @@
 # https://open.kattis.com/problems/bubbletea
-# wrong answer
+# accepted answer, CPU time: 0.06s
 
 
 """
@@ -28,33 +28,25 @@ def _get_numbers(first=False):
     return numbers[0] if first else numbers
 
 
-def find_cheapest_tea(tea_list: list) -> int:
-    min_price = float('inf')
-    for entry in tea_list:
-        tea_price = entry[0]
-        for topping in entry[1:]:
-            if tea_price + topping < min_price:
-                min_price = tea_price + topping
-    return min_price
-
-
 if __name__ == '__main__':
     from sys import stdin
 
     num_teas = _get_numbers(True)
 
-    tea_prices = [[i] for i in _get_numbers()]
+    tea_prices = _get_numbers()
 
     num_toppings = _get_numbers(True)
 
     topping_prices = _get_numbers()
 
+    cheapest = float('inf')
     for tea in tea_prices:
-        tea.extend(_get_numbers()[1:])
+        for topping in _get_numbers()[1:]:
+            total = tea + topping_prices[topping - 1]
+            if total < cheapest:
+                cheapest = total
 
-    total_money = _get_numbers(True)
+    money = _get_numbers(True)
 
-    cheapest_tea = find_cheapest_tea(tea_prices)
-    print(cheapest_tea)
-
-    print((total_money // cheapest_tea) - 1)
+    possible = max((money // cheapest) - 1, 0)
+    print(possible)
